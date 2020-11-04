fdee63d5864918a483b949dc1de088fc3df92c2b
leo <leo@localhost.localdomain>
2020-11-04 15:35:11 +0000

    wed4nov2020

--- Crash_Course_on_Python/module2_q10.py
+++ Crash_Course_on_Python/module2_q10.py
@@ -2,12 +2,8 @@ def fractional_part(numerator, denominator):
     if denominator == 0:
         part = 0
     else:
-        part = numerator % denominator
-        if part != 0:
-            part = part/denominator 
-        
-    return (part)
-
+        part = numerator / denominator - numerator//denominator
+    return part
 print(fractional_part(5, 5)) # Should be 0
 print(fractional_part(5, 4)) # Should be 0.25
 print(fractional_part(5, 3)) # Should be 0.66...
---
+++ Crash_Course_on_Python/test_modul2q10.py
@@ -0,0 +1,6 @@
+a=8//4
+b=8%4
+c=8/4
+print(a)
+print(b)
+print(c-a)
